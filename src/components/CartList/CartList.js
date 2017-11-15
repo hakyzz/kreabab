b13d328af85a0fee091ad84c37f4fb3d0c8157d0
Milo <milo@miloezger.ch>
2017-11-15 21:20:27 +0100

    cart empty state defined

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -1,40 +1,52 @@
 import React from 'react';
 
 import './CartList.css';
+import EmptyStateImg from './empty-basket.svg';
 
 import CartEntry from '../CartEntry/CartEntry';
 import Button from '../Button/Button';
 import ButtonList from '../ButtonList/ButtonList';
 
 
-const CartList = ({ cartList }) => (
-    <div className="cart-list">
-        <div className="cart-list__header">
-            <h1>Warenkorb</h1>
+const CartList = ({ cartList }) => {
+
+    const hasProducts = cartList.length > 0
+    const nodes = hasProducts ? (
+        cartList.map((cart, index) => (
+            <CartEntry
+            key={index}
+            name={cart.name}
+            image={cart.image}
+            price={cart.price}
+            />
+        ))
+    ) : (
+        <div className="cart-list__empty-state">
+            <img src={EmptyStateImg} className="empty-state-img" alt="Keine Produkte vorhanden"/>
+            <p>Keine Produkte vorhanden</p>
         </div>
-        <div className="cart-list__list">
-        {
-            cartList.map((cart, index) => (
-                <CartEntry
-                key={index}
-                name={cart.name}
-                image={cart.image}
-                price={cart.price}
-                />
-            ))
-        }
+    )
+
+    return (
+        <div className="cart-list">
+            <div className="cart-list__header">
+                <h1>Warenkorb</h1>
+            </div>
+            <div className="cart-list__list">
+                {nodes}
+            </div>
+            <div className="cart-list__summary">
+                <div className="">Total</div>
+                <div className="">CHF ### Total ###</div>
+            </div>
+            <div className="cart-list__actions">
+                <ButtonList>
+                    <Button linkName="/" className="btn cta-large secondary">Weiter Shoppen</Button>
+                    <Button linkName="/" className="btn cta-large primary">Bestellen&nbsp;&nbsp;&nbsp;></Button>
+                </ButtonList>
+            </div>
         </div>
-        <div className="cart-list__summary">
-            <div className="">Total</div>
-            <div className="">CHF ### Total ###</div>
-        </div>
-        <div className="cart-list__actions">
-            <ButtonList>
-                <Button linkName="/" className="btn cta-large secondary">Weiter Shoppen</Button>
-                <Button linkName="/" className="btn cta-large primary">Bestellen&nbsp;&nbsp;&nbsp;></Button>
-            </ButtonList>
-        </div>
-    </div>
-)
+    )
+}
 
 export default CartList;
