6e8dc053ebd257f01792edf10deb08fd08f6ba4b
Milo <milo@miloezger.ch>
2017-11-22 18:09:12 +0100

    prepared for delete function

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from '../actiontypes/ActionTypes';
+import { ADD_TO_CART, DELETE_FROM_CART } from '../actiontypes/ActionTypes';
 
 export function addToCart(product) {
   return {
@@ -6,3 +6,11 @@ export function addToCart(product) {
     payload: product
   }
 }
+
+export function deleteFromCart(itemId) {
+  return {
+    type: DELETE_FROM_CART,
+    itemId
+  }
+}
+
