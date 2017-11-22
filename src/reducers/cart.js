81d8721de61c74aa8bd73b04298f7a9da5d275da
Hakan Arioglu <hakan.arioglu@namics.com>
2017-11-22 18:41:07 +0100

    Bugfixing and style changes

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -4,26 +4,24 @@ export function cartReducer(state = {
     cartTotal: '',
     cart: []
   }, action) {
-  // console.log('cartReducer = ', action);
   switch (action.type) {
     case ADD_TO_CART:
       const elem = state.cart.find( element => element.id === action.payload.id );
       
       if ( elem ) {
-        console.log('product bereits im warenkorb');
         let newState = { ...state };
         newState.cart = state.cart.map( element => {
           if (element.id === action.payload.id) {
             element.quantity++;
             element.totalPrice = Number(element.price) * element.quantity;
           }
+          element.totalPrice = Math.round(element.totalPrice * 100) / 100;
           return element;
-        })
+        });
         newState = updateTotal(newState);
         return newState;
       
       } else {
-        console.log('product noch nicht im warenkorb');
         let newState = { ...state };
         const newItem = {
           id: action.payload.id,
@@ -32,15 +30,15 @@ export function cartReducer(state = {
           price: action.payload.price,
           totalPrice: action.payload.price,
           quantity: 1
-        }
-        newState.cart.push(newItem)
+        };
+        newState.cart.push(newItem);
         newState = updateTotal(newState);
         return newState;
       }
 
     case DELETE_FROM_CART:
       let newState4 = { ...state };
-      newState4.cart = state.cart.filter( element => element.id !== action.payload.id )
+      newState4.cart = state.cart.filter( element => element.id !== action.payload.id );
       newState4 = updateTotal(newState4);
       return newState4;
 
@@ -51,9 +49,8 @@ export function cartReducer(state = {
 
 
 function updateTotal(newState) {
-  var total = 0;
-  newState.cart.map( elem  => {'total = ', total = Number(total) + elem.quantity * elem.price } );
-  newState.cartTotal = total;
-  console.log(newState.cartTotal);
+  let total = 0;
+  newState.cart.map( elem  => {return total = Number(total) + elem.quantity * elem.price});
+  newState.cartTotal = Math.round(total * 100) / 100;
   return newState;
 }
