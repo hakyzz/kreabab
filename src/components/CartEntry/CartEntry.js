e9dcca2abbc29a6033aa84a98d16b4c2867ef48b
Hakan Arioglu <hakan.arioglu@namics.com>
2017-12-24 16:05:01 +0100

    changed input numbers to select

--- src/components/CartEntry/CartEntry.js
+++ src/components/CartEntry/CartEntry.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 
 import './CartEntry.css';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 
 export default class CartEntry extends Component {
@@ -20,15 +22,14 @@ export default class CartEntry extends Component {
     }
 
     handleInputChange(event) {
-        const target = event.target; // input number
-        const quantity = target.value;
-        const itemId = target.getAttribute("id");
-        const name = target.name; //quantity
-        
+        const quantity = event.value;
+        const itemId = event.id;
+        const name = event.name; //quantity
+
         this.setState({
             [name]: quantity
         });
-    
+
         this.props.changeQuantityCart(itemId, quantity)
     }
 
@@ -36,6 +37,15 @@ export default class CartEntry extends Component {
         function onDeleteHandler(event) {
             const itemId = event.target.getAttribute("id");
             this.props.deleteFromCart(itemId);
+        }
+
+        function createNumbersArray(number, id, name) {
+            const numbersArray = [];
+            for(let i = 1; i < number; i++) {
+                numbersArray.push({value: i, label: i, id, name});
+            }
+
+            return numbersArray;
         }
 
         return (
@@ -52,15 +62,13 @@ export default class CartEntry extends Component {
                 <div className="cart-entry__controls">
                     <div className="entry-quantity">
                         <label htmlFor="entry-quantity">Anzahl:</label>
-                        <input
-                            type="number"
-                            className="entry-quantity"
-                            id={this.props.id}
-                            min="1"
-                            name="entry-quantity"
+                        <Select
+                            clearable={false}
+                            searchable={false}
                             value={this.state.quantity}
                             onChange={this.handleInputChange}
-                            />
+                            options={createNumbersArray(999, this.props.id, "entry-quantity")}
+                        />
                     </div>
                     <div className="entry-price">
                         <span>CHF {this.props.totalPrice}</span>
