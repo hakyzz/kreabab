a1919bb110e03870d1ae3d865334575a55928f6c
Milo <milo@miloezger.ch>
2018-01-12 22:54:39 +0100

    dummy content

--- src/api/mockProducts.js
+++ src/api/mockProducts.js
@@ -1,8 +1,8 @@
 export default [
-    {
+    {   // Hauptgerichte
         id: 1,
         categoryId: "1",
-        name: "Dönerteller mit Salat 1",
+        name: "Dürüm Döner",
         image: "/uploads/01.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 12.90,
@@ -13,7 +13,7 @@ export default [
     {
         id: 2,
         categoryId: "1",
-        name: "Dönerteller mit Salat 2",
+        name: "Döner",
         image: "/uploads/02.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 9.90,
@@ -24,8 +24,8 @@ export default [
     {
         id: 3,
         categoryId: "1",
-        name: "Dönerteller mit Salat 3",
-        image: "/uploads/01.jpg",
+        name: "Dönerbox",
+        image: "/uploads/03.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 15.90,
         bred: "Taschenbrot",
@@ -35,8 +35,8 @@ export default [
     {
         id: 4,
         categoryId: "1",
-        name: "Dönerteller mit Salat 4",
-        image: "/uploads/02.jpg",
+        name: "Dönerteller",
+        image: "/uploads/04.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 20.45,
         bred: "Taschenbrot",
@@ -46,7 +46,7 @@ export default [
     {
         id: 5,
         categoryId: "1",
-        name: "Dönerteller mit Salat 5",
+        name: "Dürüm Döner",
         image: "/uploads/01.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 9.90,
@@ -57,7 +57,7 @@ export default [
     {
         id: 6,
         categoryId: "1",
-        name: "Dönerteller mit Salat 6",
+        name: "Döner",
         image: "/uploads/02.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 9.90,
@@ -65,99 +65,131 @@ export default [
         toppings: [],
         sauce: []
     },
-    {
+    {   // Salate
         id: 10,
         categoryId: "2",
-        name: "Dönerteller mit Salat 1",
-        image: "/uploads/03.png",
+        name: "Gemischter Salat",
+        image: "/uploads/10.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 12.90
     },
     {
         id: 11,
         categoryId: "2",
-        name: "Dönerteller mit Salat 1",
-        image: "/uploads/04.jpg",
+        name: "Griechischer Salat",
+        image: "/uploads/11.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 12.90
     },
     {
         id: 12,
         categoryId: "2",
-        name: "Dönerteller mit Salat 1",
-        image: "/uploads/05.png",
+        name: "Ceasar Salad",
+        image: "/uploads/12.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 12.90
     },
     {
         id: 13,
         categoryId: "2",
-        name: "Dönerteller mit Salat 1",
-        image: "/uploads/03.png",
+        name: "Caprese",
+        image: "/uploads/13.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 12.90
     },
-    {
+    {   // Dessert
         id: 20,
         categoryId: "3",
-        name: "Sorbet Zuger-Kirsch",
-        image: "/uploads/sorbet-zuger-kirsch.jpg",
+        name: "Tiramisu",
+        image: "/uploads/20.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-        price: 12.90
+        price: 10.00
     },
     {
         id: 21,
         categoryId: "3",
-        name: "Sorbet Lemon & Lime",
-        image: "/uploads/sorbet-lemon-lime.png",
+        name: "Vanille Cornet",
+        image: "/uploads/21.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-        price: 12.90
+        price: 4.50
     },
     {
         id: 22,
         categoryId: "3",
-        name: "Pingu Glace",
-        image: "/uploads/pingu-glace.png",
+        name: "Erdbeer Cornet",
+        image: "/uploads/24.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-        price: 12.90
+        price: 4.50
     },
     {
         id: 23,
         categoryId: "3",
-        name: "Tiramisu",
-        image: "/uploads/dessert-tiramisu.png",
+        name: "Sorbet Lemon & Lime",
+        image: "/uploads/22.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-        price: 12.90
+        price: 4.90
     },
     {
-        id: 30,
-        categoryId: "4",
-        name: "Bier",
-        image: "/uploads/10.jpg",
+        id: 24,
+        categoryId: "3",
+        name: "Sorbet Zuger-Kirsch",
+        image: "/uploads/23.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-        price: 12.90
+        price: 4.90
     },
-    {
+    {   // Getraenke
         id: 31,
         categoryId: "4",
-        name: "Mineralwasser",
-        image: "/uploads/11.jpg",
+        name: "Bier Calanda 5dl",
+        image: "/uploads/30.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-        price: 12.90
+        price: 5.80
     },
     {
         id: 32,
         categoryId: "4",
-        name: "Süssgetränke 5dl",
-        image: "/uploads/12.jpg",
+        name: "Mineralwasser 5dl",
+        image: "/uploads/31.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-        price: 12.90
+        price: 4.00
     },
     {
         id: 33,
         categoryId: "4",
-        name: "Süssgetränke 3dl",
-        image: "/uploads/13.jpg",
+        name: "Coca Cola 5dl",
+        image: "/uploads/32.jpg",
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+        price: 4.50
+    },
+    {
+        id: 34,
+        categoryId: "4",
+        name: "Fanta 5dl",
+        image: "/uploads/33.jpg",
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+        price: 4.50
+    },
+    {
+        id: 35,
+        categoryId: "4",
+        name: "Sprite 5dl",
+        image: "/uploads/34.jpg",
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+        price: 4.50
+    },
+    {
+        id: 36,
+        categoryId: "4",
+        name: "Red Bull 3dl",
+        image: "/uploads/35.jpg",
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+        price: 5.50
+    },
+    {
+        id: 37,
+        categoryId: "4",
+        name: "Rotwein Staatsschreiber",
+        image: "/uploads/36.jpg",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
         price: 12.90
     }
