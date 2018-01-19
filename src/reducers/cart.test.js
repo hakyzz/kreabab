dfccc4b5f12b73581a6085c085e757086d76eb61
Hakan Arioglu <hakan.arioglu@namics.com>
2018-01-19 16:03:53 +0100

    code cleanup

--- src/reducers/cart.test.js
+++ src/reducers/cart.test.js
@@ -12,40 +12,34 @@ const actionAddToCart = {
         sauces: '',
         bread: ''
     }
-}
+};
 
-const resultAddToCart = [{
-    id: '1',
-    name: 'Test',
-    image: '',
-    price: '',
-    totalPrice: '',
-    quantity: 1,
-    toppings: '',
-    sauces: '',
-    bread: ''
-}];
+const resultAddToCart = [
+    {
+        id: '1',
+        name: 'Test',
+        image: '',
+        price: '',
+        totalPrice: '',
+        quantity: 1,
+        toppings: '',
+        sauces: '',
+        bread: ''
+    }
+];
+
 describe('reducers', () => {
     describe('cart', () => {
+
         it('should return the initial state', () => {
             expect(cartReducer([], {})).toEqual(getInitialState());
         });
+
         it('should handle ADD_TODO', () => {
             expect(
                 JSON.stringify(cartReducer([], actionAddToCart))
             ).toEqual(
-                JSON.stringify([
-                    {
-                        id: '1',
-                        name: 'Test',
-                        image: '',
-                        price: '',
-                        totalPrice: '',
-                        quantity: 1,
-                        toppings: '',
-                        sauces: '',
-                        bread: ''
-                    }])
+                JSON.stringify(resultAddToCart)
             )
         });
     });
